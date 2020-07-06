dc416d35eabf5600f030db4d61cb8cc00c506f01
RTDistante <66966970+RTDistante@users.noreply.github.com>
2020-07-06 12:41:12 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,51 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace Lab4_5
+namespace Lab3_5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //1.Create a list that holds the scores you’ve been given.
-            List<int> scoreList = new List<int>();
-            scoreList.Add(4400);
-            scoreList.Add(8900);
-            scoreList.Add(1200);
-            scoreList.Add(3700);
-            scoreList.Add(7200);
-
-            Console.WriteLine("Raw Score List Data");
-            foreach (int score in scoreList)
+            string blackListWord = "viagra";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
             {
-                Console.WriteLine(score);
+                isSpam = true;
+                Console.WriteLine("The message is spam");
             }
-
-            // 2.Sort the scores lowest to highest and output to the screen.    
-            scoreList.Sort();
-            
-            Console.WriteLine("Sorted Score List Data, Low to High");
-            foreach (int score in scoreList)
+            else
             {
-                Console.WriteLine(score);
-            }
-
-            // 3.Sort the scores highest to lowest and output to the screen.
-            scoreList.Reverse();
-            
-            Console.WriteLine("Sorted Score List Data, High to Low");
-            foreach (int score in scoreList)
-            {
-                Console.WriteLine(score);
-            }
-
-            // 4.You’ve just been given a new score that wasn’t part of the original list. Add the score 2400 to the list.
-            scoreList.Add(2400);
-            
-            Console.WriteLine("New Data Added to Score List");
-            foreach (int score in scoreList)
-            {
-                Console.WriteLine(score);
+                isSpam = false;
+                Console.WriteLine("The message is not spam");
             }
         }
     }
